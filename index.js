191db4c6bafa24b24ae30f498a17cd28644a3269
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-19 16:39:47 -0600

    commit tree 43f6f57ec064b006e8dfdcf607caa14598767d4d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (activity) {
+    if (typeof activity === 'string') {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+function mondayWork(activity) {
+    if (typeof activity === 'string') {
+        return `This Monday, I will ${activity}.`
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+function wrapAdjective(flair= '*') {
+    return function(adjective= 'special') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
